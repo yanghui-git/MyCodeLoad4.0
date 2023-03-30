26b0a0ddb2fd031281d482df2be2f1a86401d0f0
yanghui <15868172375@163.com>
2023-03-30 14:51:37 +0800

    flowable流程引擎示例demo-入门

---
+++ flowable-test/src/main/java/org/test/flowable/main/FlowableApplication.java
@@ -0,0 +1,16 @@
+package org.test.flowable.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * flowable流程引擎 from Spring Boot 整合流程引擎https://juejin.cn/post/7098870076777955335#heading-6
+ */
+@SpringBootApplication
+public class FlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableApplication.class, args);
+    }
+
+}
